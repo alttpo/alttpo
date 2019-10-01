5ca18f67f6fa63e86d86624e117583047cc4e9f8
James Dunne <james.jdunne@gmail.com>
2019-10-01 16:01:14 -0500

    Using bass_v15 from byuu to create an ALTTP ROM patch to intercept NMI routine.

---
+++ asm/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp alttp.smc.bak alttp.smc; rm -f alttp.srm; bass -m alttp.smc main.asm
---
+++ asm/main.asm
@@ -0,0 +1,44 @@
+architecture wdc65816
+endian lsb
+
+// patch over header values to extend ROM size from 8Mb to 16Mb:
+{
+    //origin 0x007FC0 // <- 7FC0 - Bank00.asm : 9173 (db "THE LEGEND OF ZELDA  " ; 21 bytes)
+    //db $23, $4E
+
+    origin 0x007FD5 // <- 7FD5 - Bank00.asm : 9175 (db $20   ; rom layout)
+    //db #$35 // set fast exhirom
+    db $30 // set fast lorom
+
+    //origin 0x007FD6 // <- 7FD6 - Bank00.asm : 9176 (db $02   ; cartridge type)
+    //db $55 // enable S-RTC
+
+    origin 0x007FD7 // <- 7FD7 - Bank00.asm : 9177 (db $0A   ; rom size)
+    db $0B // mark rom as 16mbit
+
+    //origin 0x007FD8 // <- 7FD8 - Bank00.asm : 9178 (db $03   ; ram size (sram size))
+    //db $05 // mark sram as 32k
+
+    origin 0x1FFFFF // <- 1FFFFF
+    db $00 // expand file to 2mb
+}
+
+// patch over PHA : PHX : PHY : PHD within original NMI routine in bank 00:
+origin 0x0000CC
+base   0x0080CC
+    jml nmiHook
+
+// give our NMI hook somewhere to return to:
+origin 0x0000D0
+base   0x0080D0
+nmiReturn:;
+
+// our NMI hook:
+origin 0x100000
+base   0x208000
+nmiHook:
+    pha ; phx ; phy ; phd
+
+    // $7F7667[0x6719] = free RAM!
+
+    jml nmiReturn
